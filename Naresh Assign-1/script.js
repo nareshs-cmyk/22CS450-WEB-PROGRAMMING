18b42246f430b4ebc3e095e0e0866a3966d6710e
mahijith2007 <mahijith@student.tce.edu>
2026-02-18 10:33:24 +0530

    Assignment 1added

--- Naresh Assign-1/script.js
+++ Naresh Assign-1/script.js
@@ -1,6 +1,6 @@
 console.log(new Date());
 
-document.body.style.backgroundColor = "gray";
+document.body.style.backgroundColor = "";
 
 function showMsg() {
     alert("Hello");
@@ -12,19 +12,16 @@ function changeText() {
 
 function preventSubmit(event) {
     event.preventDefault();
+    var name = document.getElementById("name").value;
+    document.getElementById("demo").innerHTML = name;
 }
 
-var a   = 5;
-var b   = 10;
+var a = 5;
+var b = 10;
 var sum = a + b;
 console.log(sum);
 
 var num = 15;
 if (num > 10) {
     console.log("Greater");
-}
-
-function displayName() {
-    var name = document.getElementById("name").value;
-    document.getElementById("demo").innerHTML = name;
 }
